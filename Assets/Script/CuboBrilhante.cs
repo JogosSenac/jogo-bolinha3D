920d77e077aa5af740db7038689226999ba1c95e
Mauricio <mauricio.oliver2007@gmail.com>
2024-10-08 10:06:37 -0300

    bolinhaLisinha

---
+++ Assets/Script/CuboBrilhante.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CuboBrilhante : MonoBehaviour
+{
+    [SerializeField] private float rX; 
+    [SerializeField] private float rY; 
+    [SerializeField] private float rZ; 
+    [SerializeField] private float velocidade; 
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rX = 25f;
+        rY = 25f;
+        rZ = 25f;
+        velocidade = 5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rX * velocidade * Time.deltaTime,
+                         rY * velocidade * Time.deltaTime,  
+                         rZ * velocidade * Time.deltaTime);
+    }
+
+   
+}
